d778086bf89681c52dfa20f3fb96a7f157bf2d08
Hanul <chu9400@naver.com>
2024-06-10 21:22:37 +0900

    update - 가로 스크롤 제거 및 gsap 로딩 후 페이지 랜딩되게 수정

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,19 +1,24 @@
-// 플러그인 등록
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+// 페이지와 모든 리소스가 로드되면 애니메이션 실행
+window.addEventListener('load', () => {
+    
+    // 플러그인 등록
+    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-const horSection = gsap.utils.toArray(".scroll_item");
+    // 애니메이션 설정
+    const horSection = gsap.utils.toArray(".scroll_item");
 
-gsap.to(horSection, {
-    xPercent: -120 * (horSection.length - 1),
-    ease: "none",
-    scrollTrigger: {
-        trigger: "#section_02",
-        start: "top 0px",
-        end: "+=3000",
-        pin: true,
-        scrub: 1,
-        markers: false,
-        invalidateOnRefresh: true,
-        anticipatePin: 1,
-    },
+    gsap.to(horSection, {
+        xPercent: -120 * (horSection.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#section_02",
+            start: "top 0px",
+            end: "+=3000",
+            pin: true,
+            scrub: 1,
+            markers: false,
+            invalidateOnRefresh: true,
+            anticipatePin: 1,
+        },
+    });
 });
